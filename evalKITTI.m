f280bc3dc72f1bd7ce727c806089dc7b6e9dd59a
amilan <devnull@localhost>
2014-12-02 16:40:40 +1030

    KITTI devkit

--- evalKITTI.m
+++ evalKITTI.m
@@ -1,28 +1,35 @@
-function metricsKITTI=evalKITTI(resfile)
+function metricsKITTI=evalKITTI(infos,cls)
     % evaluate KITTI Training Set
-    % requires a file with a allInfo 520x1 struct
-    % FIX ALL THIS
+    % requires a struct array infos 1x21
     
+    metricsKITTI=zeros(1,22);
+    if length(infos)~=21
+        fprintf('wrong data');
+        return;
+    end
+    
+    if nargin<2, cls='car'; end
+    
+    cls=lower(cls);
     
     thiswd=pwd;
-    load(resfile);
     
-    pathToKittiDevkit='/home/amilan/storage/databases/KITTI/tracking/devkit_tracking/python';
+    pathToKittiDevkit='../motutils/external/KITTI/devkit_tracking/python';
     
     
-    allscen=800:820;
+    allscen=1:21;
     for scenario=allscen
-        stateInfo=allInfo(scenario,6).stateInfo;
+        stateInfo=infos(scenario).stateInfo;
         cd(thiswd)
-        tracklets=convertToKITTI(stateInfo,'Pedestrian');
+        tracklets=convertToKITTI(stateInfo,cls);
         cd(pathToKittiDevkit)
 
-        writeLabels(tracklets,'results/a/data',scenario-700);
+        writeLabels(tracklets,'results/a/data',scenario-1);
     end
     
     
     !python evaluate_tracking.py a
-    metricsKITTI=dlmread('results/a/stats_pedestrian.txt');
+    metricsKITTI=dlmread(sprintf('results/a/stats_%s.txt',cls));
     
     cd(thiswd);
 end
